685b622dfcdee6e83f35df9bbd3ba82111c63f27
Marek Miller <marek.miller@protonmail.com>
2023-08-26 20:42:13 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
 use std::{
-    collections::HashMap,
+    collections::{
+        BTreeMap,
+        HashMap,
+    },
     hash::Hash,
-    ops::AddAssign,
+    ops::{
+        AddAssign,
+        Index,
+        IndexMut,
+    },
 };
 
 use num::{
+    Complex,
     Float,
     Num,
 };
+
+#[cfg(test)]
+mod tests;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Hash, Eq)]
 pub enum Spin {
@@ -98,144 +109,318 @@ impl From<u32> for Orbital {
 }
 
 #[derive(Debug, PartialEq, Clone, Copy, Hash, Eq)]
-pub enum ElectronIntegral {
-    One((Orbital, Orbital)),
-    Two((Orbital, Orbital, Orbital, Orbital)),
+pub enum FermiCode {
+    One {
+        cr: Orbital,
+        an: Orbital,
+    },
+    Two {
+        cr: (Orbital, Orbital),
+        an: (Orbital, Orbital),
+    },
 }
 
-impl From<(Orbital, Orbital)> for ElectronIntegral {
+impl FermiCode {
+    pub fn canonical(self) -> Self {
+        match self {
+            FermiCode::One { cr, an } => { 
+                if cr.h() <= an.h() {
+                    self
+                } else {
+                    FermiCode::One { an, cr}
+                }
+            }
+            FermiCode::Two { cr, an } => {
+                let 
+            }
+        }
+    }
+}
+
+impl From<(Orbital, Orbital)> for FermiCode {
     fn from(value: (Orbital, Orbital)) -> Self {
-        Self::One(value)
+        Self::One {
+            cr: value.0,
+            an: value.1,
+        }
     }
 }
 
-impl From<(Orbital, Orbital, Orbital, Orbital)> for ElectronIntegral {
+impl From<(Orbital, Orbital, Orbital, Orbital)> for FermiCode {
     fn from(value: (Orbital, Orbital, Orbital, Orbital)) -> Self {
-        Self::Two(value)
+        Self::Two {
+            cr: (value.0, value.1),
+            an: (value.2, value.3),
+        }
     }
 }
 
-impl ElectronIntegral {
+impl FermiCode {
     pub fn symmetries(self) -> impl Iterator<Item = Self> {
-        Symmetries::new(self)
+        ElectronSyms::new(self)
+    }
+}
+
+impl IntoIterator for FermiCode {
+    type IntoIter = ElectronOrbitals;
+    type Item = Orbital;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ElectronOrbitals::new(self)
     }
 }
 
 #[derive(Debug)]
-pub struct Symmetries {
-    integral: ElectronIntegral,
-    index:    u8,
+pub struct ElectronOrbitals {
+    idx:   FermiCode,
+    count: u8,
 }
 
-impl Symmetries {
-    pub fn new(integral: ElectronIntegral) -> Self {
+impl ElectronOrbitals {
+    pub fn new(idx: FermiCode) -> Self {
         Self {
-            integral,
-            index: 0,
+            idx,
+            count: 0,
         }
     }
 }
 
-impl Iterator for Symmetries {
-    type Item = ElectronIntegral;
+impl Iterator for ElectronOrbitals {
+    type Item = Orbital;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index == 0 {
-            self.index += 1;
-            return Some(self.integral);
-        }
-
-        let item = match self.integral {
-            ElectronIntegral::One(h) => {
-                if self.index == 1 {
-                    Some(ElectronIntegral::One((h.1, h.0)))
-                } else {
-                    None
-                }
-            }
-            ElectronIntegral::Two(h) => {
-                let tuple = match self.index {
-                    1 => Some((h.0, h.1, h.3, h.2)),
-                    2 => Some((h.1, h.0, h.2, h.3)),
-                    3 => Some((h.1, h.0, h.3, h.2)),
-                    4 => Some((h.2, h.3, h.0, h.1)),
-                    5 => Some((h.2, h.3, h.1, h.0)),
-                    6 => Some((h.3, h.2, h.1, h.0)),
-                    7 => Some((h.3, h.2, h.0, h.1)),
-                    _ => None,
-                };
-
-                tuple.map(ElectronIntegral::Two)
-            }
+        let item = match self.idx {
+            FermiCode::One {
+                cr,
+                an,
+            } => match self.count {
+                0 => Some(cr),
+                1 => Some(an),
+                _ => None,
+            },
+            FermiCode::Two {
+                cr,
+                an,
+            } => match self.count {
+                0 => Some(cr.0),
+                1 => Some(cr.1),
+                2 => Some(an.0),
+                3 => Some(an.1),
+                _ => None,
+            },
         };
-        self.index += 1;
+        self.count += 1;
+        if self.count >= 4 {
+            self.count = 4;
+        }
         item
     }
 }
 
-#[derive(Debug, Default, PartialEq, Clone, Copy, Hash)]
+#[derive(Debug)]
+pub struct ElectronSyms {
+    idx:   FermiCode,
+    count: u8,
+}
+
+impl ElectronSyms {
+    pub fn new(integral: FermiCode) -> Self {
+        Self {
+            idx:   integral,
+            count: 0,
+        }
+    }
+}
+
+impl Iterator for ElectronSyms {
+    type Item = FermiCode;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = match self.idx {
+            FermiCode::One {
+                cr,
+                an,
+            } => match self.count {
+                0 => Some((cr, an).into()),
+                1 => Some((an, cr).into()),
+                _ => None,
+            },
+
+            FermiCode::Two {
+                cr,
+                an,
+            } => match self.count {
+                0 => Some((cr.0, cr.1, an.0, an.1).into()),
+                1 => Some((cr.0, cr.1, an.1, an.0).into()),
+                2 => Some((cr.1, cr.0, an.0, an.1).into()),
+                3 => Some((cr.1, cr.0, an.1, an.0).into()),
+                4 => Some((an.0, an.1, cr.0, cr.1).into()),
+                5 => Some((an.0, an.1, cr.1, cr.0).into()),
+                6 => Some((an.1, an.0, cr.1, cr.0).into()),
+                7 => Some((an.1, an.0, cr.0, cr.1).into()),
+                _ => None,
+            },
+        };
+        self.count += 1;
+        if self.count >= 8 {
+            self.count = 8
+        }
+        item
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Clone, Copy, Hash, Eq)]
 pub enum Pauli {
     #[default]
-    I = 0,
-    X = 1,
-    Y = 2,
-    Z = 3,
+    I,
+    X,
+    Y,
+    Z,
+}
+
+#[derive(Debug, PartialEq, Clone, Hash, Eq)]
+pub struct PauliCode(BTreeMap<u32, Pauli>);
+
+impl Default for PauliCode {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl PauliCode {
+    pub fn new() -> Self {
+        Self(BTreeMap::new())
+    }
+}
+
+impl From<&[Pauli]> for PauliCode {
+    fn from(value: &[Pauli]) -> Self {
+        let mut code = PauliCode::new();
+        for (i, pauli) in value.iter().enumerate() {
+            code.0.insert(i as u32, *pauli);
+        }
+        code
+    }
+}
+
+pub trait Conj {
+    fn conj(self) -> Self;
+}
+
+impl<T> Conj for Complex<T>
+where
+    T: Float,
+{
+    fn conj(self) -> Self {
+        Complex::conj(&self)
+    }
+}
+
+impl Conj for PauliCode {
+    fn conj(self) -> Self {
+        self
+    }
+}
+
+impl Conj for FermiCode {
+    fn conj(self) -> Self {
+        match self {
+            FermiCode::One {
+                cr,
+                an,
+            } => (an, cr).into(),
+            FermiCode::Two {
+                cr,
+                an,
+            } => (an.1, an.0, cr.1, cr.0).into(),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Hamiltonian<K, T> {
-    num_qubits: u32,
-    coeffs:     HashMap<K, T>,
+    terms: HashMap<K, T>,
 }
 
 impl<K, T> Hamiltonian<K, T> {
-    pub fn new(num_qubits: u32) -> Self {
+    pub fn new() -> Self {
         Self {
-            num_qubits,
-            coeffs: HashMap::new(),
+            terms: HashMap::new(),
         }
-    }
-
-    pub fn as_hashmap(&self) -> &HashMap<K, T> {
-        &self.coeffs
-    }
-
-    pub fn as_hashmap_mut(&mut self) -> &mut HashMap<K, T> {
-        &mut self.coeffs
     }
 }
 
-type FermionHamiltonian<T> = Hamiltonian<ElectronIntegral, T>;
+type FermiHamil<T> = Hamiltonian<FermiCode, T>;
 
-impl<T> FermionHamiltonian<T>
+impl<T> FermiHamil<T>
 where
     T: Copy + Hash + Eq,
 {
+    /// # Panics
+    ///
+    /// Panics if `idx` is out of bound, i.e. `orbital.h() >= N`
+    /// for any `orbital` in `idx`
     pub fn update(
         &mut self,
-        integral: ElectronIntegral,
+        code: FermiCode,
         value: T,
     ) {
-        for x in integral.symmetries() {
-            self.coeffs.insert(x, value);
-        }
+        self.terms.insert(code.canonical(), value);
     }
 }
 
-impl<T> FermionHamiltonian<T>
+impl<T> FermiHamil<T>
 where
     T: AddAssign + Copy + Hash + Eq,
 {
     pub fn add(
         &mut self,
-        integral: ElectronIntegral,
+        code: FermiCode,
         value: T,
     ) {
-        for x in integral.symmetries() {
-            self.coeffs
-                .entry(x)
-                .and_modify(|coeff| *coeff += value)
-                .or_insert(value);
-        }
+        self.terms
+            .entry(code.canonical())
+            .and_modify(|coeff| *coeff += value)
+            .or_insert(value);
+    }
+}
+
+type PauliHamil<T> = Hamiltonian<PauliCode, T>;
+
+impl<T> PauliHamil<T>
+where
+    T: Hash + Eq,
+{
+    pub fn update(
+        &mut self,
+        code: PauliCode,
+        value: T,
+    ) {
+        self.terms.insert(code, value);
+    }
+}
+
+impl<T> PauliHamil<T>
+where
+    T: AddAssign + Copy + Hash + Eq,
+{
+    pub fn add(
+        &mut self,
+        code: PauliCode,
+        value: T,
+    ) {
+        self.terms
+            .entry(code)
+            .and_modify(|coeff| *coeff += value)
+            .or_insert(value);
+    }
+}
+
+impl<T> From<FermiHamil<T>> for PauliHamil<T>
+where
+    T: Float,
+{
+    fn from(hamil: FermiHamil<T>) -> Self {
+        todo!()
     }
 }
